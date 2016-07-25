679a07d1ebde87bafd1b658cb5dad9083b259a15
Julio Marquez <juliocmrqzm@gmail.com>
2016-07-25 13:41:49 -0400

    Bump version to 0.1.0

--- knowledgebase/__init__.py
+++ knowledgebase/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 __author__ = 'Julio Marquez'
 __copyright__ = 'Copyright (c) 2016 Julio Marquez'
 __license__ = 'Apache License 2.0'
-__version__ = '0.1.0a'
+__version__ = '0.1.0'
 
 default_app_config = 'knowledgebase.apps.DjangoKnowledgebaseConfig'
