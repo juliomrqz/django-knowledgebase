0238a6f476a28e784687295ffc4c7bac50779059
Julio Marquez <juliocmrqzm@gmail.com>
2016-09-05 20:51:23 -0400

    Fix JavaSscript issues in demo

--- example/example/static/js/app.js
+++ example/example/static/js/app.js
@@ -1,36 +1,50 @@
 $(function() {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  $("[data-toggle='tooltip']").tooltip();
+});
 
 // Voting
-var csrftoken = getCookie('csrftoken');
+// Get a cookie by a name
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
+// Get csrftoken cookie value
+var csrftoken = getCookie("csrftoken");
+
+// Send csrftoken value to every Ajax request
 $.ajaxSetup({
-  beforeSend: function(xhr, settings) {
+  beforeSend(xhr) {
     if (!this.crossDomain) {
       xhr.setRequestHeader("X-CSRFToken", csrftoken);
     }
   }
 });
 
-
+// Voting instruction for articles
 $("#upvote-link, #downvote-link").click(function(e) {
-
   e.preventDefault();
-
 
   $.ajax({
     type: "POST",
-    url: $(this).attr('href'),
+    url: $(this).attr("href"),
     async: false,
-    success: function(result) {
+    success(result) {
       $('#voting-box').html(result.message);
-      console.log(result);
     },
-    error: function(result) {
+    error(result) {
       $('#voting-box').html(result.message);
-      console.log(result);
     }
   });
-
 });
